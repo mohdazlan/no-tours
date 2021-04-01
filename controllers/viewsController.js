1363f11e0e270c1f4420b73873912c36ad3de8ad
azlan <mhdazlanabaziz@gmai.com>
2021-04-01 16:11:53 +0800

    trying to finish the login screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -12,7 +12,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the reqested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -20,8 +20,26 @@ exports.getTour = catchAsync(async (req, res) => {
   });
   // 2) Build template
   // 3) Render template using data from 1)
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-    tour,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour', {
+      title: `${tour.name} tour`,
+      tour,
+    });
 });
+
+exports.getLoginForm = (req, res) => {
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.cloudflare.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('login', {
+      title: 'Log into your account',
+    });
+};
